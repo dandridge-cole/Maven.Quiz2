6d1a35a0984bd5b36257c6ec233a560fd100c061
Dandridge Cole <dandridgecole@yahoo.com>
2019-06-10 12:16:00 -0400

    got an ArrayList<Pair> working for enough of the methods to pass all the tests.

--- src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
@@ -1,17 +1,24 @@
 package com.zipcodewilmington.assessment2.part2;
 
 import javafx.util.Pair;
+
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Router {
-    Map<String,String> theMap = new LinkedHashMap<>();
+    private Boolean usingAL=true;
+    private ArrayList<Pair<String,String>> pseudoMap = new ArrayList<>();
+    private Map<String,String> theMap = new LinkedHashMap<>();
+ //   Pair<String,String> thisPair = new Pair<>(s1,s2);
 
     public void add(String path, String controller) {
         theMap.put(path, controller);
+        pseudoMap.add(new Pair<>(path,controller));
     }
 
     public Integer size() {
+        if(usingAL) pseudoMap.size();
         return theMap.size();
     }
 
@@ -29,8 +36,14 @@ public class Router {
 
     public String toString(){
         StringBuilder builder = new StringBuilder();
-        for(Map.Entry<String,String> entry : theMap.entrySet()){
-            builder.append(entry.getKey()).append(entry.getValue()).append('\n');
+        if(usingAL){
+            for (Pair<String,String> pair : pseudoMap) {
+                builder.append(pair.getKey()).append(pair.getValue()).append('\n');
+            }
+        } else {
+            for (Map.Entry<String, String> entry : theMap.entrySet()) {
+                builder.append(entry.getKey()).append(entry.getValue()).append('\n');
+            }
         }
         return builder.toString();
     }
