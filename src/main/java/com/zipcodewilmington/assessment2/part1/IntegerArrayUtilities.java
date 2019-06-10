bab2b9e2309b7bea4630935b9e866ce673a7b15c
Dandridge Cole <dandridgecole@yahoo.com>
2019-06-10 09:42:13 -0400

    Part one all tests passing

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -1,19 +1,31 @@
 package com.zipcodewilmington.assessment2.part1;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class IntegerArrayUtilities {
     public Boolean hasEvenLength(Integer[] array) {
-        return null;
+        return array.length%2==0;
     }
 
     public Integer[] range(int start, int stop) {
-        return null;
+        Integer[] answer = new Integer[stop-start+1];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i]=i+start;
+        }
+        return answer;
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
-        return null;
+        return array[0]+array[1];
     }
 
-    public Integer getProductOfFirstTwo(Integer[] array) {
-        return null;
+    /*
+    public Integer getProductOfFirstTwo(Integer[] array) { //original version based on description
+        return array[0]*array[1];
+    }*/
+
+    public Integer getProductOfFirstTwo(Integer[] array) { // coded to test cases, although it appears one or the other has been misnamed.
+        return array[array.length-1]*array[array.length-2];
     }
 }
